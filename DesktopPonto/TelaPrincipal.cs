5567ca492069d5d0c626891e17c08e763d0fe109
gaugsouza <guilhermeguis@outlook.com>
2020-10-15 22:08:16 -0300

    ajustes finais

--- DesktopPonto/TelaPrincipal.cs
+++ DesktopPonto/TelaPrincipal.cs
@@ -205,5 +205,10 @@ namespace DesktopPonto
             }
             this.setaValores();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
